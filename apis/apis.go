af7016a55d2bdfc5ba8abd51bed274507b2027b3
younaman <52592519+younaman@users.noreply.github.com>
2023-11-08 16:29:24 +0800

    Add files via upload

--- apis/apis.go
+++ apis/apis.go
@@ -1,1 +1,34 @@
+package apis
 
+type Pod struct {
+	Namespace      string
+	Name           string
+	ServiceAccount string
+	ControllBy     []string
+	TokenMounted   bool
+}
+
+type ServiceAccount struct {
+	namespace string
+	name      string
+	roles     []Role
+}
+
+type RoleBinding struct {
+	Namespace string
+	Name      string
+	RoleRef   string
+	Subject   []string
+}
+
+type Role struct {
+	namespace string
+	name      string
+	rules     []Rule
+}
+
+type Rule struct {
+	// deployments deployments(coredns)
+	Resourcs []string
+	Verbs    []string
+}
